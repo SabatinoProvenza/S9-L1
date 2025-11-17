079c5eb65e1f3f6d7859b886582fd2593d97bcd8
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-11-17 15:07:03 +0100

    creates ImageComponent

--- S9-L1/src/App.jsx
+++ S9-L1/src/App.jsx
@@ -1,9 +1,12 @@
 import ButtonComponent from "./components/ButtonComponent"
+import ImageComponent from "./components/ImageComponent"
+import milanLogo from "./assets/milan.png"
 
 function App() {
   return (
     <>
       <ButtonComponent name="Sabatino" />
+      <ImageComponent src={milanLogo} alt={"logo Milan"} />
     </>
   )
 }
